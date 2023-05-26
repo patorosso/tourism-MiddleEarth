e191d0ba2d74b7daa8b45073050e2da04ab90e90
i7-6700 <i7-6700@DESKTOP-8D6K6DO>
2023-05-26 17:04:41 -0300

    Mejora de codigo al buscar oferta dentro de las atracciones

--- src/data/models/ManejadorDeCupos.java
+++ src/data/models/ManejadorDeCupos.java
@@ -12,24 +12,34 @@ public class ManejadorDeCupos {
 	}
 	
 	public boolean tengoCupoPara(final Oferta oferta) {
-		for (Atraccion atraccion : atracciones) {
-			if (atraccion.getNombre().equals(oferta.getNombre())) {
-				return atraccion.getCupos() > 0;
-			}
+		final Atraccion atraccion = buscarOferta(oferta);
+		
+		if (atraccion != null) {
+			return atraccion.getCupos() > 0;
 		}
 		
 		return false; 
 	}
 	
 	public void restarCupo(final Oferta oferta) {
-		for (Atraccion atraccion : atracciones) {
-			if (atraccion.getNombre().equals(oferta.getNombre())) {
-				final int cupos = atraccion.getCupos();
-				if (cupos > 0) {
-					atraccion.setCupos(cupos - 1);
-				}
+		final Atraccion atraccion = buscarOferta(oferta);
+		
+		if (atraccion != null) {
+			final int cupos = atraccion.getCupos();
+			if (cupos > 0) {
+				atraccion.setCupos(cupos - 1);
 			}
 		}
+	}
+	
+	private Atraccion buscarOferta(final Oferta oferta) {
+		for (Atraccion atraccion : atracciones) {
+			if (atraccion.getNombre().equals(oferta.getNombre())) {
+				return atraccion;
+			}
+		}
+		
+		return null;
 	}
 	
 }
