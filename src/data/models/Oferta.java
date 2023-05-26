62b94be266f1b9590c766ea4a2af1ee4a787916d
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-05-26 00:52:19 -0300

    add Archivo and ManejoArchivo classes. A lot commented till Promocion class is ready

--- src/data/models/Oferta.java
+++ src/data/models/Oferta.java
@@ -4,12 +4,36 @@ public abstract class Oferta {
 	
 	protected String nombre;
 	protected int costo;
-	protected float duracion;
+	protected double duracion;
 	
-	public Oferta(String nombre, int costo, float duracion) {
+	public Oferta(String nombre, int costo, double duracion) {
 		
 		this.nombre = nombre;
 		this.costo = costo;
+		this.duracion = duracion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCosto() {
+		return costo;
+	}
+
+	public void setCosto(int costo) {
+		this.costo = costo;
+	}
+
+	public double getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(double duracion) {
 		this.duracion = duracion;
 	}
 
